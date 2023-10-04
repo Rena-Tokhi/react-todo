1887cc83717f440a042cc28481be8e498e1e9d57
Rina-Tokhi <Rina.Tokhi7@gmail.com>
2023-10-04 11:21:58 -0700

    List added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+const todoList = [
+  { 
+    id: 1,
+    title: "Complete assignment 1" 
+  },
+  { 
+    id: 2,
+    title: "Read a book" 
+  },
+  { 
+    id: 3,
+    title: "Go for a run"
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
