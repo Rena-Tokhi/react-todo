ff9eb678c81a925e0e4c9a4794a82628b6b1863f
Rina-Tokhi <Rina.Tokhi7@gmail.com>
2023-10-18 15:51:07 -0700

    Lesson1_3

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 
 const todoList = [
     {
@@ -19,7 +20,7 @@ function TodoList (){
     return(
         <ul>
         {todoList.map((item) =>
-          <li key={item.id}>{item.title}</li>
+          <TodoListItem key={item.id} todo={item} />
         )}
       </ul>
     );
